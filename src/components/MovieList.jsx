271a330628c0edb4df49e6b7599ffc55602ed68e
bonface221 <mainaboniface221@gmail.com>
2022-12-21 17:23:22 +0300

    ft:added the movie list component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
 
 export class MovieList extends Component {
 	state = {
@@ -9,29 +9,47 @@ export class MovieList extends Component {
 	render() {
 		return (
 			<div className="container mt-3">
-				<table className="table">
-					<thead>
-						<tr>
-							<th scope="colspan-2">Title</th>
-							<th scope="col">Genre</th>
-							<th scope="col">Stock</th>
-							<th scope="col">Rate</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.movies.map((movie) => (
-							<tr>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<button className="btn btn-danger">Delete</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
+				{this.state.movies.length > 0 ? (
+					<div>
+						<p>Showing {this.state.movies.length} movies in the database</p>
+						<table className="table">
+							<thead>
+								<tr>
+									<th scope="colspan-2">Title</th>
+									<th scope="col">Genre</th>
+									<th scope="col">Stock</th>
+									<th scope="col">Rate</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.movies.map((movie) => (
+									<tr key={movie._id}>
+										<td>{movie.title}</td>
+										<td>{movie.genre.name}</td>
+										<td>{movie.numberInStock}</td>
+										<td>{movie.dailyRentalRate}</td>
+										<td>
+											<button
+												onClick={() =>
+													this.setState({
+														movies: this.state.movies.filter(
+															(m) => m._id !== movie._id
+														),
+													})
+												}
+												className="btn btn-danger"
+											>
+												Delete
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				) : (
+					<p> there are no movies to show sorry</p>
+				)}
 			</div>
 		);
 	}
